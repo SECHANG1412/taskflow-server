f5d7cedb4e907693d5038d86096697082adcf6d3
sechang <binaryblitz1412@gmail.com>
2026-06-05 10:44:00 +0900

    fix: uvicorn 실행 환경 의존성 문제 해결

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,5 +1,3 @@
-import email
-from opcode import hasexc
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
